2474cdfce5e67f789ace7f2bbf8847930695dfe4
Jason <jasondylan2002@gmail.com>
2025-08-05 21:02:25 -0400

    Update 2

---
+++ src/main/java/net/insigmatic/theendermaster/TheEnderMaster.java
@@ -0,0 +1,16 @@
+package net.insigmatic.theendermaster;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+    //Very, very, VERY important comment!!
+public class TheEnderMaster implements ModInitializer {
+	public static final String MOD_ID = "theendermaster";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
